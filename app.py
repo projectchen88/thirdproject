e78ad96439c053be523e16c11018a53e96681fa9
Ubuntu <ubuntu@ip-172-31-95-136.ec2.internal>
2020-04-01 09:11:09 +0000

    index function setup with templates and static folder

---
+++ app.py
@@ -0,0 +1,32 @@
+import os, pymongo
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+MONGO_URI = os.getenv('MONGO_URI')
+DATABASE_NAME = 'recipe'
+COLLECTION_FOOD_RECIPE = 'food_recipe'
+
+
+# Connect to Mongo
+conn = pymongo.MongoClient(MONGO_URI)
+# Set to link to Mongo database
+data_food_recipe = conn[DATABASE_NAME][COLLECTION_FOOD_RECIPE]
+
+
+@app.route('/') # map the root route to the index function
+def index():
+    result = data_food_recipe.find({})
+    return render_template ('index.html'
+    ,data = result
+    )
+
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
