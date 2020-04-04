9082b02f4987afeb9422137ed3a6152d4e5f55e3
Ubuntu <ubuntu@ip-172-31-95-136.ec2.internal>
2020-04-04 17:56:24 +0000

    added requirement.txt file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web gunicorm app:app
--- app.py
+++ app.py
@@ -409,6 +409,6 @@ if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
     
-    app.run(host=os.environ.get('IP','0.0.0.0'),
-            port=int(os.environ.get('PORT','5000')),
-            debug=False)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
